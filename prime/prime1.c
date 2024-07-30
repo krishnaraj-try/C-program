cc34c0c6222a6e8c7babd4a4fb65f878300d3d2f
Krishnaraj R <133135310+krishnaraj-try@users.noreply.github.com>
2024-07-30 19:37:26 +0530

    Update prime1.c

--- prime/prime1.c
+++ prime/prime1.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int i,num;
+  printf("ENTER THE NUMBER:\n");
+  scanf("%d",&num);
+  for(i=2;i<num;i++)
+    {
+      if(num%i==0)
+        break;
+    }
+  if(num==i)
+    printf("PRIME\n");
+  else
+    puts("NOT PRIME");
+  
+  
+}
 
